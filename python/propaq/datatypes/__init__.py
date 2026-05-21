904331ba221c2f2b78b818fc70b4b0022acf00cb
Hrishikesh Belagali <belagal1@msu.edu>
2026-05-21 17:30:35 -0400

    Add TermSum to __init__

--- python/propaq/datatypes/__init__.py
+++ python/propaq/datatypes/__init__.py
@@ -2,5 +2,6 @@
 
 from .pauli import PauliTerm as PauliTerm
 from .majorana import MajoranaMonomial as MajoranaMonomial
+from .termsum import TermSum as TermSum 
 
-__all__ = ["PauliTerm", "MajoranaMonomial"]
+__all__ = ["PauliTerm", "MajoranaMonomial", "TermSum"]
