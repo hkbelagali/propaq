c43e7479e174a6520993b50c740d1ff5a2f6228b
Hrishikesh Belagali <belagal1@msu.edu>
2026-05-22 00:09:05 -0400

    Majorana circuit parser from qiskit

--- propaq/circuits/majorana/circuit.py
+++ propaq/circuits/majorana/circuit.py
@@ -1,11 +1,15 @@
 """Circuit representation for fermionic circuits in the Majorana representation."""
 
-from typing import List
+from typing import TYPE_CHECKING, List
+
+import qiskit 
+
 
 from qiskit import QuantumCircuit 
 from ffsim.qiskit import PrepareHartreeFockJW, UCJOpSpinBalancedJW
     
 from ...datatypes.majorana import MajoranaMonomial
+from ...datatypes.termsum import TermSum
 from .rotation import MajoranaRotation
 
 class MajoranaCircuit: 
@@ -33,43 +37,55 @@ class MajoranaCircuit:
         Since we will be applying the circuit in the Heisenberg picture, the state 
         should be prepared separately and the circuit should only contain the 
         forward evolution. 
+
+        Here, each of the supported gates will be translated into a TermSum of 
+        MajoranaMonomials, which will then be converted into MajoranaRotations.
         """
 
         generators: List[MajoranaMonomial] = [] 
         angles: List[float] = [] 
 
         for instr, qargs, _ in qc.data: 
-            if instr.name not in ["xx_plus_yy", "p", "cp", "swap", "measure", "barrier"]:
-                raise ValueError(f"Unsupported gate {instr.name} in Qiskit circuit. Only xx_plus_yy, p, cp, x, and swap are supported.")
+            if instr.name in ["measure", "barrier"]:
+                continue
+            if instr.name not in ["xx_plus_yy", "p", "rz", "cp", "swap"]:
+                raise ValueError(f"Unsupported gate {instr.name} in Qiskit circuit. Supported gates: xx_plus_yy, p, rz, cp, swap.")
 
+            q_indices = [qc.find_bit(q).index for q in qargs]
+            
             if instr.name == "xx_plus_yy":
                 if len(qargs) != 2:
                     raise ValueError("xx_plus_yy gate must have exactly 2 qubits.")
-                pass
-
-            elif instr.name == "p": 
-                pass 
-
-            elif instr.name == "cp": 
-                pass
+                majoranasum: TermSum[MajoranaMonomial] = TermSum[MajoranaMonomial].from_xx_plus_yy(instr, q_indices, n_modes) 
                 
-            elif instr.name == "swap": 
-                pass 
+            elif instr.name == "p": 
+                majoranasum = TermSum[MajoranaMonomial].from_phase(instr, q_indices, n_modes) 
+                
+            elif instr.name == "rz": 
+                majoranasum = TermSum[MajoranaMonomial].from_rz(instr, q_indices, n_modes) 
+
+            elif instr.name == "cp":
+                if len(qargs) != 2:
+                    raise ValueError("cp gate must have exactly 2 qubits.")
+                majoranasum = TermSum[MajoranaMonomial].from_cp(instr, q_indices, n_modes)
+
+            elif instr.name == "swap":
+                raise NotImplementedError("swap gate lacks a TermSum constructor.")
+
+            for gen, ang in majoranasum.items():
+                generators.append(gen)
+                angles.append(float(ang.real))
         
         return cls.from_generators_and_angles(generators, angles, n_modes) 
-
 
     @classmethod 
     def lucj_from_ffsim(cls, lucj: UCJOpSpinBalancedJW):
         """Construct a MajoranaCircuit from an ffsim UCJOpSpinBalancedJW and PrepareHartreeFockJW."""
         # TODO: Implementation for converting ffsim circuit to Majorana representation
-        pass    
+        raise NotImplementedError("Conversion from ffsim to MajoranaCircuit is not yet implemented. Convert the ffsim circuit to Qiskit and use the from_qiskit class method.")
 
-    def __reversed__(self): 
-        """
-        Return a new MajoranaCircuit with the order of rotations reversed and angles negated. 
-        This is needed when we go to the Heisenberg picture and need to apply the inverse of the circuit.
-        """
+    def inverse(self):
+        """Return a new MajoranaCircuit with reversed order and negated angles (U†)."""
         reversed_rotations = [MajoranaRotation(rot.generator, -rot.angle) for rot in reversed(self.rotations)]
         return MajoranaCircuit(reversed_rotations, self.n_modes)
     
