022ccd7b6e25e01972ab674558327204194b1e52
Hrishikesh Belagali <belagal1@msu.edu>
2026-07-25 20:16:48 -0400

    Add hydrogen chain benchmarks

--- benchmarks/bench_ucj.py
+++ benchmarks/bench_ucj.py
@@ -1,12 +1,22 @@
-import numpy as np
+"""
+Propagation-timing benchmarks for the LUCJ ansatz on linear hydrogen chains.
+"""
 
-DAMPING = 0.001325
-WEIGHT_CUTOFF = 100000
-COEFF_CUTOFF = 1e-16
-TRUNCATION_RANGE = (100_000, 1_000_000)
+import os
+
+os.environ.setdefault("JAX_PLATFORMS", "cpu")
+
+ATOM = "H"
+ATOMIC_DISTANCE = 1.0
+BASIS = "sto-6g"
+NLAYERS = 1
 
 
-def _build_lucj_circuit(natoms, nlayers):
+def _generate_linear_geometry(natoms: int) -> str:
+    return "; ".join([f"{ATOM} 0 0 {i * ATOMIC_DISTANCE}" for i in range(natoms)])
+
+
+def _build_lucj_circuit(natoms: int):
     """Compile a LUCJ ansatz circuit for a linear hydrogen chain."""
     import ffsim
     import pyscf
@@ -15,47 +25,30 @@ def _build_lucj_circuit(natoms, nlayers):
     from qiskit.providers.fake_provider import GenericBackendV2
     from qiskit.transpiler import CouplingMap
 
-    geometry = "; ".join([f"H 0 0 {i * 1.0}" for i in range(natoms)])
     mol = pyscf.gto.Mole()
     mol.verbose = 0
-    mol.build(atom=geometry, basis="sto-6g")
+    mol.build(atom=_generate_linear_geometry(natoms), basis=BASIS)
 
-    active_space = range(mol.nao_nr())
     scf = pyscf.scf.RHF(mol)
     scf.verbose = 0
     scf.run()
 
-    norb = len(active_space)
-    n_electrons = int(sum(scf.mo_occ[active_space]))
-    n_alpha = (n_electrons + mol.spin) // 2
-    n_beta = (n_electrons - mol.spin) // 2
-    nelec = (n_alpha, n_beta)
+    norb = mol.nao_nr()
+    n_electrons = int(sum(scf.mo_occ))
+    nelec = (n_electrons // 2, n_electrons // 2)
 
     ccsd = pyscf.cc.CCSD(scf)
     ccsd.verbose = 0
     ccsd.run()
-    t1, t2 = ccsd.t1, ccsd.t2
-
-    coupling_map = CouplingMap.from_grid(
-        num_rows=int(np.ceil(np.sqrt(2 * norb))),
-        num_columns=int(np.ceil(np.sqrt(2 * norb))),
-    )
-    backend = GenericBackendV2(
-        coupling_map.size(),
-        coupling_map=coupling_map,
-        basis_gates=["cp", "xx_plus_yy", "p", "x", "swap"],
-    )
-
-    pairs_aa = [(p, p + 1) for p in range(norb - 1)]
-    pairs_ab = [(p, p) for p in range(norb)]
 
     ucj_op = ffsim.UCJOpSpinBalanced.from_t_amplitudes(
-        t2=t2,
-        t1=t1,
-        n_reps=nlayers,
-        interaction_pairs=(pairs_aa, pairs_ab),
-        optimize=True,
-        options=dict(maxiter=100),
+        t2=ccsd.t2, t1=ccsd.t1, n_reps=NLAYERS, optimize=True, options=dict(maxiter=1000),
+    )
+
+    coupling_map = CouplingMap.from_full(2 * norb, bidirectional=True)
+    backend = GenericBackendV2(
+        2 * norb, coupling_map=coupling_map,
+        basis_gates=["cp", "xx_plus_yy", "p", "x", "swap"],
     )
 
     qubits = qiskit.QuantumRegister(2 * norb, name="q")
@@ -63,76 +56,64 @@ def _build_lucj_circuit(natoms, nlayers):
     circuit.append(ffsim.qiskit.PrepareHartreeFockJW(norb, nelec), qubits)
     circuit.append(ffsim.qiskit.UCJOpSpinBalancedJW(ucj_op), qubits)
 
-    return qiskit.transpile(circuit, backend=backend, optimization_level=3)
+    return qiskit.transpile(
+        circuit, backend=backend, optimization_level=3, initial_layout=list(range(2 * norb)),
+    )
 
 
-class LUCJMajoranaBench:
-    timeout = 600
-    params = [[2, 4], [1]]
-    param_names = ["natoms", "nlayers"]
+class MajoranaUCJBench:
+    """Majorana propagation timing for the LUCJ ansatz on a hydrogen chain."""
 
-    def setup(self, natoms, nlayers):
-        try:
-            import ffsim  # noqa: F401
-        except ImportError:
-            raise NotImplementedError("ffsim not installed")
+    timeout = 300
+    params = [[2, 4, 6]]
+    param_names = ["natoms"]
+
+    def setup(self, natoms):
+        from qiskit.quantum_info import SparsePauliOp
 
         from propaq.circuits import MajoranaCircuit
         from propaq.datatypes import MajoranaTermSum
         from propaq.noise import TruncationPolicy
-        from qiskit.quantum_info import SparsePauliOp
 
-        compiled = _build_lucj_circuit(natoms, nlayers)
+        compiled = _build_lucj_circuit(natoms)
         n_qubits = compiled.num_qubits
-        self.circuit = MajoranaCircuit.from_qiskit(
-            compiled.copy(), n_modes=2 * n_qubits
-        )
+        self.circuit = MajoranaCircuit.from_qiskit(compiled.copy(), n_modes=2 * n_qubits)
         observable = SparsePauliOp("ZZ" + "I" * (n_qubits - 2))
         self.obs = MajoranaTermSum.from_sparse_pauli_op(observable)
-        self.trunc = TruncationPolicy(
-            weight_cutoff=WEIGHT_CUTOFF, coeff_cutoff=COEFF_CUTOFF,
-            truncation_range=TRUNCATION_RANGE,
-        )
+        self.trunc = TruncationPolicy(weight_cutoff=100_000, coeff_cutoff=1e-10)
 
-    def time_expectation_value(self, natoms, nlayers):
-        from propaq.noise import UniformNoiseModel
+    def time_expectation_value(self, natoms):
         from propaq.propagators import MajoranaPropagator
 
-        MajoranaPropagator(
-            UniformNoiseModel(damping=DAMPING), self.trunc, n_threads=1
-        ).expectation_value(self.obs, self.circuit, fock_state=0)
+        MajoranaPropagator(None, self.trunc, n_threads=1).expectation_value(
+            self.obs, self.circuit, initial_state=0
+        )
 
 
-class LUCJPauliBench:
-    timeout = 600
-    params = [[2, 4], [1]]
-    param_names = ["natoms", "nlayers"]
+class PauliUCJBench:
+    """Pauli propagation timing for the LUCJ ansatz on a hydrogen chain."""
 
-    def setup(self, natoms, nlayers):
-        try:
-            import ffsim  # noqa: F401
-        except ImportError:
-            raise NotImplementedError("ffsim not installed")
+    timeout = 300
+    params = [[2, 4, 6]]
+    param_names = ["natoms"]
+
+    def setup(self, natoms):
+        from qiskit.quantum_info import SparsePauliOp
 
         from propaq.circuits import PauliCircuit
         from propaq.datatypes import PauliTermSum
         from propaq.noise import TruncationPolicy
-        from qiskit.quantum_info import SparsePauliOp
 
-        compiled = _build_lucj_circuit(natoms, nlayers)
+        compiled = _build_lucj_circuit(natoms)
         n_qubits = compiled.num_qubits
         self.circuit = PauliCircuit.from_qiskit(compiled.copy())
         observable = SparsePauliOp("ZZ" + "I" * (n_qubits - 2))
         self.obs = PauliTermSum.from_sparse_pauli_op(observable)
-        self.trunc = TruncationPolicy(
-            weight_cutoff=WEIGHT_CUTOFF, coeff_cutoff=COEFF_CUTOFF,
-            truncation_range=TRUNCATION_RANGE,
-        )
+        self.trunc = TruncationPolicy(weight_cutoff=100_000, coeff_cutoff=1e-10)
 
-    def time_expectation_value(self, natoms, nlayers):
-        from propaq.noise import UniformNoiseModel
+    def time_expectation_value(self, natoms):
         from propaq.propagators import PauliPropagator
 
-        PauliPropagator(
-            UniformNoiseModel(damping=DAMPING), self.trunc, n_threads=1
-        ).expectation_value(self.obs, self.circuit, fock_state=0)
+        PauliPropagator(None, self.trunc, n_threads=1).expectation_value(
+            self.obs, self.circuit, initial_state=0
+        )
