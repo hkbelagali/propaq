628d68c275eb66cebb25900854ce6fd688c74ef7
Hrishikesh Belagali <belagal1@msu.edu>
2026-08-12 01:06:42 -0400

    ruff linting

--- propaq/extrapolators/__init__.py
+++ propaq/extrapolators/__init__.py
@@ -1,8 +1,8 @@
 """Extrapolation techniques for Heisenberg simulations."""
 
-from .zce import ZeroCutoffExtrapolator as ZeroCutoffExtrapolator
 from .zce import CoefficientCutoffExtrapolator as CoefficientCutoffExtrapolator
 from .zce import WeightCutoffExtrapolator as WeightCutoffExtrapolator
 from .zce import ZCEResult as ZCEResult
+from .zce import ZeroCutoffExtrapolator as ZeroCutoffExtrapolator
 from .zne import ZeroNoiseExtrapolator as ZeroNoiseExtrapolator
 from .zne import ZNEResult as ZNEResult
