142851103814370c0e25f5ea01bcb89009b118ad
Hrishikesh Belagali <belagal1@msu.edu>
2026-09-06 12:05:39 -0400

    Make rotations inherit from AbstractRotation

--- propaq/circuits/pauli/rotation.py
+++ propaq/circuits/pauli/rotation.py
@@ -1,9 +1,10 @@
 """Gate parameterization for circuits in the Pauli representation."""
 
 from ...datatypes.pauli.pauli import PauliString
+from ..abstract import AbstractRotation
 
 
-class PauliRotation:
+class PauliRotation(AbstractRotation[PauliString]):
     r"""
     Class representing a gate in the Pauli representation, parameterized by a Pauli string and a rotation angle.
 
@@ -21,30 +22,3 @@ class PauliRotation:
 
     generator: PauliString
     """The Pauli string \\(P\\) that generates the rotation."""
-
-    angle: float
-    r"""The rotation angle \(\theta\) in radians."""
-
-    is_intermediate: bool
-    """
-    Whether this rotation is an intermediate parameterization that does not preserve particle number. This is used to control
-    truncation policies during propagation.
-    """
-
-    qiskit_gate_idx: int | None
-    """Index of the originating Qiskit gate in the source circuit, or None for non-Qiskit circuits."""
-
-    def __init__(
-        self,
-        generator: PauliString,
-        angle: float,
-        is_intermediate: bool = False,
-        qiskit_gate_idx: int | None = None,
-    ):
-        """
-        Construct a PauliRotation from a generator and an angle.
-        """
-        self.generator = generator
-        self.angle = angle
-        self.is_intermediate = is_intermediate
-        self.qiskit_gate_idx = qiskit_gate_idx
